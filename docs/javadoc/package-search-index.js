c6ebb82b714b2024305fd4d116cde8c7f47c93b6
Jumman04 <arsharifuddinjumman04@gmail.com>
2025-05-08 22:34:36 +0600

    docs: generate Javadoc for DataManager and related classes

--- docs/javadoc/package-search-index.js
+++ docs/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jummania"},{"l":"com.jummania.converter"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jummania"},{"l":"com.jummania.converter"},{"l":"com.jummania.model"}];updateSearchResults();
